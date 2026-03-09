457863427526536add33e35c70c681e444f38329
rachad <rachdos76@gmail.com>
2026-03-09 14:19:26 +0000

    creer livreController et authController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LivreController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/livres', [LivreController::class, 'index']);
