357bbd98f98d2f3038f7aafb5ffcc2ce6793ca45
rachad <rachdos76@gmail.com>
2026-03-09 12:29:15 +0000

    creer les modals livre category exemplaire

--- app/Models/Exemplaire.php
+++ app/Models/Exemplaire.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exemplaire extends Model
 {
-    //
+    protected $fillable = ['livre_id', 'etat_physique', 'est_disponible'];
+
+    public function livre() {
+        return $this->belongsTo(Livre::class);
+    }
 }
--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Livre extends Model
 {
-    //
+    protected $fillable = ['titre', 'auteur', 'category_id', 'vues'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function exemplaires() {
+        return $this->hasMany(Exemplaire::class);
+    }
 }
