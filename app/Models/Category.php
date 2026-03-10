a70e67de8c1e80b3731d714f9ecb42d74c877315
rachad <rachdos76@gmail.com>
2026-03-10 20:26:33 +0000

    fix un probleme des colonnes dans category modal

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['livre_id', 'etat_physique', 'est_disponible'];
+    protected $fillable = ['nom', 'description'];
 
     public function livre() {
         return $this->belongsTo(Livre::class);
