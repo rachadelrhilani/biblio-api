60b669609fa701cf8807f88f5e72d5da746c3b31
rachad <rachdos76@gmail.com>
2026-03-13 10:10:25 +0000

    modifier seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-       
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test1@example.com',
-            "password"=> "password",
-        ]);
+         $this->call([
+        CategorySeeder::class,
+        LivreSeeder::class,
+    ]);
     }
 }
